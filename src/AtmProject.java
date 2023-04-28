3b68a8d27db93e051e30f86a072c9f6ee008d2a6
yusuf <yatakan89@gmail.com>
2023-04-28 16:20:46 -0400

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,6 +2,8 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("bu bir atm projesidir");
         //version1
+        System.out.println("java is easy");
+        //version2
     }
 
 }
